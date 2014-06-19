3c5d257bfc39ed3659600161f91281490291637a
idonotknowit <sihaidada@gmail.com>
2014-06-19 21:48:01 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We use the first matrix to create a global matrix, then we can use the second function
+## to compute its inverse. If the inverse has existed, we just use the ecomputed one.
 
-## Write a short comment describing this function
+## We use this function to create a sample case of inversible matrix
+y <- matrix()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        x <<- matrix(c(1 0 0 2),nrow=2,ncol=2)
 }
 
 
-## Write a short comment describing this function
+## This function is used to compute the inverse of a given matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        dim_m <- dim(y)
+        if (sum(dim_m)==2 & y[1,1]==NA) {y <<- solve(x)} 
+        return(y)
 }
